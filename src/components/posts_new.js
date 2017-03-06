a0735f8740e601e7cd25a6c288cc9bf401f76ef2
William Dunlop <william.dunlop.1993@gmail.com>
2017-03-06 17:46:54 +1100

    S6L91

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { reduxForm } from 'redux-form';
-import {createPost } from '../actions/index';
+import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends React.Component {
+  static contextTypes = {
+    router: React.PropTypes.object
+  };
+
+onSubmit(props) {
+  this.props.createPost(props)
+    .then(() => {
+      //blog post has been created
+      // navigate the user to the index
+      this.context.router.push('/');
+    });
+}
+
   render() {
     const { fields: { title, categories, content}, handleSubmit } = this.props;
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create Post</h3>
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
